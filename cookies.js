b6bfb7ec1df052879456f02b405a656be2837b5a
Pierre Cabrière <hello@pierrecabriere.fr>
2018-03-01 04:27:46 +0100

    ready for production !

---
+++ cookies.js
@@ -0,0 +1,57 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Cookies = function () {
+  function Cookies() {
+    _classCallCheck(this, Cookies);
+  }
+
+  _createClass(Cookies, null, [{
+    key: "set",
+    value: function set(name, value, config) {
+      var opts = _extends({
+        source: document,
+        path: '/'
+      }, config);
+
+      var c = name + "=" + value + ";";
+      if (opts.days) {
+        var d = new Date();
+        d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * opts.days);
+        c += "expires=" + d.toGMTString() + ";";
+      }
+      if (opts.path) {
+        c += "path=" + opts.path + ";";
+      }
+      opts.source.cookie = c;
+    }
+  }, {
+    key: "get",
+    value: function get(name, source) {
+      source = !source ? document : source;
+      var v = source.cookie && source.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+      return v ? v[2] : null;
+    }
+  }, {
+    key: "delete",
+    value: function _delete(name, source) {
+      this.set(name, '', {
+        days: -1,
+        source: source
+      });
+    }
+  }]);
+
+  return Cookies;
+}();
+
+exports.default = Cookies;
